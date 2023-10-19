cf4b8f010ca780fc2f4ec4a0483b03a9e63b7540
sazi-d45 <sazid3993@gmail.com>
2023-10-19 11:36:24 +0600

    benar section ready

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+
+var container = document.getElementById('container')
+
+var heading = document.getElementById('heading')
+
 function responsiveManu(){
 
     document.getElementById('header-manu').classList.toggle('Active');
 }
+
+
+
+
+window.addEventListener('scroll', function(){
+    var scrolledAmount = scrollY;
+    if(scrolledAmount > 100){
+        heading.style.backgroundColor = '#3A3E64';
+        container.style.borderBottom = 'none';
+    }
+
+    else{
+        heading.style.backgroundColor = 'transparent';
+        container.style.borderBottom = '1px solid rgba(253, 252, 252 ,0.2)';
+    }
+})
